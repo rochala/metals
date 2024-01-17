8f0a4c984e1d5ad2d541542db55bae2154c9c734
Wojciech Mazur <wmazur@virtuslab.com>
2024-01-17 17:33:04 +0100

    Add ClientInfo to MetalsLspContext

--- metals/src/main/scala/scala/meta/internal/metals/MetalsLspService.scala
+++ metals/src/main/scala/scala/meta/internal/metals/MetalsLspService.scala
@@ -2855,6 +2855,9 @@ class MetalsLspService(
       /* serverConfig = */ telemetry.conversion.MetalsServerConfig(
         serverInputs.initialServerConfig
       ),
+      /* clientInfo =*/ telemetry.conversion.MetalsClientInfo(
+        initializeParams.getClientInfo()
+      ),
       /* buildServerConnections = */ bspSession.toList
         .flatMap(
           telemetry.conversion.BuildServerConnections(_)
