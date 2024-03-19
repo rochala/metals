69c708ff5551d43f71e7c5fbf2ade65f65cd1d9e
rochala <rochala@tutanota.com>
2024-03-19 12:10:31 +0100

    Move to scala classes from java interfaces, push for better overview of changes

--- metals/src/main/scala/scala/meta/internal/metals/Compilers.scala
+++ metals/src/main/scala/scala/meta/internal/metals/Compilers.scala
@@ -1144,10 +1144,10 @@ class Compilers(
       config: PresentationCompilerConfig,
       options: List[String],
   ): ReporterContext =
-    new ScalaPresentationCompilerContext(
-      scalaVersion,
-      options.asJava,
-      telemetry.conversion.PresentationCompilerConfig(config),
+    ScalaPresentationCompilerContext(
+      scalaVersion = scalaVersion,
+      options = options,
+      config = telemetry.conversion.PresentationCompilerConfig(config),
     )
 
   private def remoteReporting(
